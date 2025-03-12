e33c2b926187003c45c4a66637b53a99e0d815ba
JoyadityaNath <joyadityan@gmail.com>
2025-03-12 23:43:22 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-png("plot1.png", height = 480, width = 480)
+png("plot1.png",height = 480, width = 480)
 hist(filtered$Global_active_power, 
      main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)",
